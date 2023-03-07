83b6f46500d09ef146771e64d0543274a3b90814
haskek <hasan_keklik1@hotmail.com>
2023-03-07 17:00:44 +0300

    Hasan Keklik S15G1

--- api/server.js
+++ api/server.js
@@ -1,16 +1,19 @@
+
 const express = require("express");
+
 const helmet = require("helmet");
+
 const cors = require("cors");
+
+const userModel = require("./users/users-model");
 
 /**
   Kullanıcı oturumlarını desteklemek için `express-session` paketini kullanın!
   Kullanıcıların gizliliğini ihlal etmemek için, kullanıcılar giriş yapana kadar onlara cookie göndermeyin. 
   'saveUninitialized' öğesini false yaparak bunu sağlayabilirsiniz
   ve `req.session` nesnesini, kullanıcı giriş yapana kadar değiştirmeyin.
-
   Kimlik doğrulaması yapan kullanıcıların sunucuda kalıcı bir oturumu ve istemci tarafında bir cookiesi olmalıdır,
   Cookienin adı "cikolatacips" olmalıdır.
-
   Oturum memory'de tutulabilir (Production ortamı için uygun olmaz)
   veya "connect-session-knex" gibi bir oturum deposu kullanabilirsiniz.
  */
@@ -18,8 +21,42 @@ const cors = require("cors");
 const server = express();
 
 server.use(helmet());
+
 server.use(express.json());
+
 server.use(cors());
+
+const session = require("express-session");
+
+const SessionStore = require("connect-session-knex")(session);
+
+const userRouter = require("./users/users-router");
+
+const authRouter = require("./auth/auth-router");
+
+
+server.use(session({
+  name:"cikolatacips",
+  secret:"secret_cikolatacips",
+  cookie:{
+    maxAge:1000*60*60,
+    secure:false
+  },
+  store:new SessionStore({
+    knex : require("../data/db-config"),
+    tableName:"sessions",
+    sidFieldName:"sid",
+    createTable:true,
+    clearInterval:1000*60*60
+  }),
+  resave:false,
+  saveUninitialized:false
+}));
+
+server.use("/api/users",userRouter);
+
+server.use("/api/auth",authRouter);
+
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
